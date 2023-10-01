c7d8464762e996be367ab2c05b391019b7b1ed7a
Jorge Espinoza <Jorge.Espinoza.Lossada>
2023-10-01 12:27:05 -0300

    funcinando el script para ejecutar local

--- toExecLocal.sh
+++ toExecLocal.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 #Variables
-source_file="serverip.csv"
-server_user="admin"
-key_name="ubuntuServers.pem"
-currentDate="$(date +"%m-%d_%Y_%N")"
-tmpfolder="/tmp"
-while IFS=',' read -r ipdir
+srcfl="serverip.csv"
+srvusr="admin@"
+knm="ubuntuServers.pem"
+cdt="$(date +"%m-%d_%Y_%N")"
+tmpf=':/tmp/'
+fltu="pruebalogs.sh"
+
+while IFS=',' read -r ipdir fldr
 do
+     #echo "copiando el file a $ipdir"
 
-   # echo "copiando al server $ipdir"
-    echo sudo scp -i $key_name pruebalogs.sh $server_user@$ipdir:/tmp
-    # sudo scp -i ubuntuServers.pem admin@3.95.161.64:/tmp
-    #echo "conectado al server $ipdir"
-    #ssh -i $key_name $server_user@$ipdir
-    # ssh -i ubuntuServers.pem admin@3.95.161.64
+    
+    cmd="sudo scp -i $knm $fltu $srvusr$ipdir$fldr"
+    #cmd="sudo scp -i $knm $fltu $srvusr$ipdir$tmpf" 
+     
+     #cmd="sudo scp -i "ubuntuServers.pem" pruebalogs.sh admin@3.91.184.172:/tmp"
 
+     echo $cmd
+     #$cmd
 
+    #ssh -i $knm $srvusr@$ipdir
+    #ls -lh /tmp/
 
-echo $ipdir
-done < "$source_file"
+done < "$srcfl"
