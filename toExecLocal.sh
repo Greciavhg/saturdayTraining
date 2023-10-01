ae483c5bab95cfd570ef488bbdce7e38008718aa
Jorge Espinoza <Jorge.Espinoza.Lossada>
2023-09-30 23:27:26 -0300

    1127pm last update

--- toExecLocal.sh
+++ toExecLocal.sh
@@ -4,12 +4,12 @@ source_file="serverip.csv"
 server_user="admin"
 key_name="ubuntuServers.pem"
 currentDate="$(date +"%m-%d_%Y_%N")"
-
+tmpfolder="/tmp"
 while IFS=',' read -r ipdir
 do
 
-    echo "copiando al server $ipdir"
-    sudo scp -i $key_name pruebalogs.sh $server_user@$ipdir:/tmp
+   # echo "copiando al server $ipdir"
+    echo sudo scp -i $key_name pruebalogs.sh $server_user@$ipdir:/tmp
     # sudo scp -i ubuntuServers.pem admin@3.95.161.64:/tmp
     #echo "conectado al server $ipdir"
     #ssh -i $key_name $server_user@$ipdir
@@ -19,5 +19,3 @@ do
 
 echo $ipdir
 done < "$source_file"
-
-E: The repository 'http://us.archive.ubuntu.com/ubuntu vivid Release' does not have a Release file.
