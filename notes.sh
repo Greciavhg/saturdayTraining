c9c49866b0dadf7416804c2a63d23be781ac1b58
Jorge Espinoza <Jorge.Espinoza.Lossada>
2023-09-30 21:11:34 -0300

    ultimo before change

---
+++ notes.sh
@@ -0,0 +1,16 @@
+# 1 Un servidor con un script que reviente el disco
+
+# 2 script pra correr en todos los servers
+
+# 3 un server de paginas webs
+
+# prubas scp
+scp archivo-a-copiar usuario@direccion-del-servidor:directorio-de-destino/
+scp -i "trainingkeys.pem" pruebalogs.sh admin@34.235.157.115:/tmp
+
+# Para el aliad
+alias grecia='cd /mnt/c/Users/Gre/OneDrive/Escritorio/Study/Linux/Training\ Grecia\ CV/Sabado30/'
+
+# Connect
+ssh -i "trainingkeys.pem" admin@34.235.157.115
+
